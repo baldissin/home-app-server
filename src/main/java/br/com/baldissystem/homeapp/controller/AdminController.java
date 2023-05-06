ed027701c09cfa82bbabcef111ea60c3ec895643
baldissin <baldissinallan@gmail.com>
2023-05-06 18:22:39 -0300

    start home-app-server

---
+++ src/main/java/br/com/baldissystem/homeapp/controller/AdminController.java
@@ -0,0 +1,11 @@
+package br.com.baldissystem.homeapp.controller;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AdminController {
+
+}
