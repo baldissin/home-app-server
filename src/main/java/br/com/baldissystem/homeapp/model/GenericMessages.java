a1da618ff7c7892edd3ae0980fa880e02277c866
baldissin <baldissinallan@gmail.com>
2023-05-15 20:53:15 -0300

    solve sonarlint errors

--- src/main/java/br/com/baldissystem/homeapp/model/GenericMessages.java
+++ src/main/java/br/com/baldissystem/homeapp/model/GenericMessages.java
@@ -2,10 +2,14 @@ package br.com.baldissystem.homeapp.model;
 
 public class GenericMessages {
 
-    public static String SAVED = "Salvo com sucesso.";
-    public static String PERSON_NOT_FOUND = "Pessoa não encontrada.";
-    public static String WELCOME = "Bem vindo(a) ";
-    public static String WRONG_PASSWORD = "Senha incorreta.";
-    public static String USER_NOT_FOUND = "Usuário não encontrado.";
+    private GenericMessages() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String SAVED = "Salvo com sucesso.";
+    public static final String PERSON_NOT_FOUND = "Pessoa não encontrada.";
+    public static final String WELCOME = "Bem vindo(a) ";
+    public static final String WRONG_PASSWORD = "Senha incorreta.";
+    public static final String USER_NOT_FOUND = "Usuário não encontrado.";
 
 }
